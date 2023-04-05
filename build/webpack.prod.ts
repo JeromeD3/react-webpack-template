c1dcc0d041837622a8da886ab474e356b5d459bf
邓祥柏 <1760399176@qq.com>
2023-04-05 17:44:04 +0800

    feat: css 压缩

--- build/webpack.prod.ts
+++ build/webpack.prod.ts
@@ -1,27 +1,33 @@
 //  打包环境
-import path from "path";
-import { Configuration } from "webpack";
-import { merge } from "webpack-merge";
-import CopyPlugin from "copy-webpack-plugin";
-import baseConfig from "./webpack.base";
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+import path from 'path'
+import { Configuration } from 'webpack'
+import { merge } from 'webpack-merge'
+import CopyPlugin from 'copy-webpack-plugin'
+import baseConfig from './webpack.base'
+import  MiniCssExtractPlugin  from 'mini-css-extract-plugin'
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
 
 const prodConfig: Configuration = merge(baseConfig, {
-  mode: "production", // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
+  mode: 'production', // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "../public"), // 复制public下文件
-          to: path.resolve(__dirname, "../dist"), // 复制到dist目录中
-          filter: (source) => !source.includes("index.html"), // 忽略index.html
+          from: path.resolve(__dirname, '../public'), // 复制public下文件
+          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
+          filter: (source) => !source.includes('index.html'), // 忽略index.html
         },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: 'static/css/[name].css' // 抽离css的输出目录和名称
+      filename: 'static/css/[name].css', // 抽离css的输出目录和名称
     }),
   ],
-});
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(), // 压缩css
+    ],
+  },
+})
 
-export default prodConfig;
+export default prodConfig
