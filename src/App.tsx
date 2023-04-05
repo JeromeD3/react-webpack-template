524ca8834bf1dfc6921c81a2b160f550ff4ec2bd
邓祥柏 <1760399176@qq.com>
2023-04-05 15:16:28 +0800

    feat: 处理图片

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import lessStyles from './app.less'
 import scssStyles from './app.scss'
 import stylStyles from './app.styl'
 
+import smallImg from '@/assets/imgs/1.jpg'
+import bigImg from '@/assets/imgs/image-20230209005812767.png'
+
 function App() {
   return (
     <div>
       <h2>webpack5-react-ts</h2>
       <div className={lessStyles['lessBox']}>
         <div className={lessStyles['box']}>lessBox</div>
+      </div>
+      <div>
+        <img src={smallImg} alt="" />
+        <img src={bigImg} alt="" />
+
       </div>
       <div className={scssStyles['scssBox']}>
         <div className={scssStyles['box']}>scssBox</div>
--- typings/global.d.ts
+++ typings/global.d.ts
@@ -34,3 +34,33 @@ declare module '*.styl' {
   const classes: { [key: string]: string }
   export default classes
 }
+/* IMAGES */
+declare module '*.svg' {
+  const ref: string
+  export default ref
+}
+
+declare module '*.bmp' {
+  const ref: string
+  export default ref
+}
+
+declare module '*.gif' {
+  const ref: string
+  export default ref
+}
+
+declare module '*.jpg' {
+  const ref: string
+  export default ref
+}
+
+declare module '*.jpeg' {
+  const ref: string
+  export default ref
+}
+
+declare module '*.png' {
+  const ref: string
+  export default ref
+}
