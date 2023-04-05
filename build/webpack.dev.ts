b487a79ce74893af3d0cde768c31d086f93a7949
邓祥柏 <1760399176@qq.com>
2023-04-05 11:27:31 +0800

    feat: 重启项目时在同一个浏览器Tab中打开页面

--- build/webpack.dev.ts
+++ build/webpack.dev.ts
@@ -1,13 +1,20 @@
 //  开发环境配置
 import path from 'path'
 import { merge } from 'webpack-merge'
-import { Configuration as WebpackConfiguration } from 'webpack'
-import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
+import webpack, { Configuration as WebpackConfiguration } from 'webpack'
+// import WebpackDevServer from 'webpack-dev-server'
+import WebpackDevServer, { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
 import baseConfig from './webpack.base'
 
 interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration
 }
+
+// 运行命令的时候重启一次打开一个tab 页很烦，所以呢优化一下
+// 参考：create-react-app 的启动方式
+// https://github.com/facebook/create-react-app/blob/main/packages/react-dev-utils/openChrome.applescript
+// 记得关闭webpack-dev-server的配置中的自动打开 open: false 或者注释
+const openBrowser = require('./utils/openBrowser')
 
 const host = '127.0.0.1'
 const port = '8082'
@@ -15,11 +22,20 @@ const port = '8082'
 // 合并公共配置,并添加开发环境配置
 const devConfig: Configuration = merge(baseConfig, {
   mode: 'development', // 开发模式,打包更加快速,省了代码优化步骤
+  /**
+    开发环境推荐：eval-cheap-module-source-map
+    - 本地开发首次打包慢点没关系,因为 eval 缓存的原因, 热更新会很快
+    - 开发中,我们每行代码不会写的太长,只需要定位到行就行,所以加上 cheap
+    - 我们希望能够找到源代码的错误,而不是打包后的,所以需要加上 module
+   */
   devtool: 'eval-cheap-module-source-map', // 可以定位到源代码的错误位置
-  devServer: {
+})
+
+const devServer = new WebpackDevServer(
+  {
     host,
     port,
-    open: true, // 是否自动打开
+    open: false, // 是否自动打开
     compress: false, // gzip压缩,开发环境不开启，提升热更新速度
     hot: true, // 开启热更新
     historyApiFallback: true, // 解决history路由404问题
@@ -29,6 +45,12 @@ const devConfig: Configuration = merge(baseConfig, {
     },
     headers: { 'Access-Control-Allow-Origin': '*' },
   },
+  webpack(devConfig)
+)
+
+devServer.start().then(() => {
+  // 启动界面
+  openBrowser(`http://${host}:${port}`)
 })
 
 export default devConfig
