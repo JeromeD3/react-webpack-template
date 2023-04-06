12f54fefbe067766e6e51dc738b46b305ce11ee2
邓祥柏 <1760399176@qq.com>
2023-04-06 22:14:25 +0800

    chore: commit的相关配置

---
+++ .versionrc.js
@@ -0,0 +1,17 @@
+// hidden 属性值控制是否将该类型的 commit 消息写入 changlog, 不填的情况下默认是: false，在根目录下新建 .versionrc.js：
+module.exports = {
+  types: [
+    { type: 'feat', section: '✨ Features | 新功能' },
+    { type: 'fix', section: '🐛 Bug Fixes | Bug 修复' },
+    { type: 'init', section: '🎉 Init | 初始化' },
+    { type: 'docs', section: '✏️ Documentation | 文档' },
+    { type: 'style', section: '💄 Styles | 风格' },
+    { type: 'refactor', section: '♻️ Code Refactoring | 代码重构' },
+    { type: 'perf', section: '⚡ Performance Improvements | 性能优化' },
+    { type: 'test', section: '✅ Tests | 测试' },
+    { type: 'revert', section: '⏪ Revert | 回退', hidden: true },
+    { type: 'build', section: '📦‍ Build System | 打包构建' },
+    { type: 'chore', section: '🚀 Chore | 构建/工程依赖/工具' },
+    { type: 'ci', section: '👷 Continuous Integration | CI 配置' }
+  ]
+}
